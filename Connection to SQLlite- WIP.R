a453064e994ad99cfb7a59008c7fc8b52505b92a
Michael Dewey <michael.dewey131@gmail.com>
2020-04-29 20:17:25 -0700

    WIP for figuring out SQL integration

---
+++ Connection to SQLlite- WIP.R
@@ -0,0 +1,40 @@
+library(RMySQL)
+
+options(mysql = list(
+  "host" = "127.0.0.1",
+  "port" = ,
+  "user" = "",
+  "password" = ""
+))
+databaseName <- "Snappa Scoreboard"
+table <- "players"
+
+saveData <- function(data) {
+  # Connect to the database
+  db <- dbConnect(MySQL(), dbname = databaseName, host = options()$mysql$host, 
+                  port = options()$mysql$port, user = options()$mysql$user, 
+                  password = options()$mysql$password)
+  # Construct the update query by looping over the data fields
+  query <- sprintf(
+    "INSERT INTO %s (%s) VALUES ('%s')",
+    table, 
+    paste(names(data), collapse = ", "),
+    paste(data, collapse = "', '")
+  )
+  # Submit the update query and disconnect
+  dbGetQuery(db, query)
+  dbDisconnect(db)
+}
+
+loadData <- function() {
+  # Connect to the database
+  db <- dbConnect(MySQL(), dbname = databaseName, host = options()$mysql$host, 
+                  port = options()$mysql$port, user = options()$mysql$user, 
+                  password = options()$mysql$password)
+  # Construct the fetching query
+  query <- sprintf("SELECT * FROM %s", table)
+  # Submit the fetch query and disconnect
+  data <- dbGetQuery(db, query)
+  dbDisconnect(db)
+  data
+}
