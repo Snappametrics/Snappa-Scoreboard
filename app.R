339ae3a713bf215c08c02487d24b06fe57fd2ca0
MokeEire <markkbarrett@gmail.com>
2020-05-08 15:55:08 -0700

    Add scoring buttons to the font size increase

--- app.R
+++ app.R
@@ -125,7 +125,7 @@ ui <- fluidPage(
   # includeCSS("www/bootstrap.css"),
   # includeCSS("www/styles.css"),
   useShinyjs(),
-  tags$style("#start_game, #previous_round, #next_round, #a_score_button, #b_score_button { font-size: 130% }"),
+  tags$style("#start_game, #previous_round, #next_round, #a_score_button, #b_score_button, #scorer, #paddle, #score { font-size: 130% }"),
   # Switching mechanism
   tags$style("#switcher { display:none; }"),
   # Application title
