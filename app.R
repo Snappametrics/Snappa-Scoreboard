5094df87e2c19c5a5eeff9768f1f353d58cc5dfa
mdewey131 <michael.dewey131@gmail.com>
2020-10-18 15:51:43 -0700

    replaces team_summary_tab with test_function (to be renamed). Adds an observer for pressing the new button allowing mid-game checks

--- app.R
+++ app.R
@@ -704,11 +704,11 @@ server <- function(input, output, session) {
   
 
   output$team_a_summary = render_gt({
-    team_summary_tab(vals$player_stats_db, player_stats_tbl, vals$players_db, "A")
+    test_function(vals$player_stats_db, player_stats_tbl, vals$players_db, snappaneers(), "A", as.numeric(str_sub(round_num(), 1, -2)), scores_tbl)
   })  
   
   output$team_b_summary = render_gt({
-    team_summary_tab(vals$player_stats_db, player_stats_tbl, vals$players_db, "B")
+    test_function(vals$player_stats_db, player_stats_tbl, vals$players_db, snappaneers(), "B", as.numeric(str_sub(round_num(), 1, -2)), scores_tbl)
   })  
   
   
@@ -1136,8 +1136,17 @@ observe({
   }, once = T, ignoreNULL = T)
   
 
+observeEvent(input$score_breakdown, {
+  showModal(
+    tags$div(id= "game_summary", 
+             game_summary(replace_na(vals$game_stats_db, list(points_a = vals$current_scores$team_A, 
+                                                              points_b = vals$current_scores$team_B)))
+    )
+  )
+  
+})
 
-
+  
   
 # Restart a game after indicating you would like to do so
   observeEvent(input$resume_yes, {
