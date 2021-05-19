9068d959485e8aab157c0f5700c2cf0032156fb2
MokeEire <markkbarrett@gmail.com>
2021-05-19 13:26:19 -0700

    Clean up app_update_player_stats function, rename to aggregate_player_stats
    - Remove troll_check because it is no longer needed

--- server_functions.R
+++ server_functions.R
@@ -2,10 +2,10 @@
 # writes player_stats_db off of scores. This should only require
 # a scores table to be passed on, since the snappaneers table that is also
 # called would always be the same
-app_update_player_stats = function(scores_df, neers, game){
-  output = scores_df %>% 
+aggregate_player_stats = function(scores_df, snappaneers, game){
+  scores_df %>% 
     # Join scores to snappaneers to get each player's team
-    right_join(neers, by = "player_id") %>% 
+    right_join(snappaneers, by = "player_id") %>% 
     # Fill in game_id for players who have not scored yet
     replace_na(list(game_id = game)) %>% 
     # Group by game and player, (team and shots are held consistent)
@@ -20,19 +20,15 @@ app_update_player_stats = function(scores_df, neers, game){
               clink_points = sum(points_scored * clink),
               points_per_round = na_if(total_points / last(shots), Inf),
               off_ppr = sum(points_scored * !(paddle | foot)) / last(shots), 
-              def_ppr = na_if(sum(points_scored * (paddle | foot)) /last(shots), Inf),
-              toss_efficiency = sum(!(paddle | foot)) / last(shots), 
+              def_ppr = na_if(sum(points_scored * (paddle | foot)) / last(shots), Inf),
+              toss_efficiency = sum((points_scored>0) * !(paddle | foot)) / last(shots), 
               .groups = "drop") %>% 
     # Replace NA values with 0s
     replace_na(list(total_points = 0, 
                     ones = 0, twos = 0, threes = 0, impossibles = 0, 
                     paddle_points = 0, clink_points = 0, 
                     points_per_round = 0, off_ppr = 0, def_ppr = 0, toss_efficiency = 0))
-  
-  output = troll_check(snappaneers = neers,
-                       player_stats = output,
-                       game_id = game)
-  return(output)
+
 }
 
 toss_percent_plus = function(x){
@@ -145,54 +141,8 @@ validate_scores = function(player, shot, snappaneers, paddle,
   
 }
 
-troll_check = function(session, snappaneers, player_stats, game_id){
-  
-  
-  trolls = snappaneers %>%
-    anti_join(player_stats, by = "player_id")
-  
-  bind_rows(player_stats, 
-            tibble(
-              game_id = rep(game_id, times = nrow(trolls)),
-              player_id = pull(trolls, player_id), 
-              team = pull(trolls, team), 
-              total_points = rep(integer(1), times = nrow(trolls)), # Weirdly enough, integer(1) is a 0 integer vector of length 1
-              shots = pull(trolls, shots),
-              ones = rep(integer(1), times = nrow(trolls)),
-              twos = rep(integer(1), times = nrow(trolls)),
-              threes = rep(integer(1), times = nrow(trolls)),
-              impossibles = rep(integer(1), times = nrow(trolls)),
-              paddle_points = rep(integer(1), times = nrow(trolls)),
-              clink_points = rep(integer(1), times = nrow(trolls)),
-              points_per_round = rep(double(1), times = nrow(trolls)),
-              off_ppr = rep(double(1), times = nrow(trolls)),
-              def_ppr = rep(double(1), times = nrow(trolls)),
-              toss_efficiency = rep(double(1), times = nrow(trolls))
-            )
-  )
-  
-}
 
-aggregate_player_stats = function(scores, players){
-  scores %>% 
-    # Join scores to snappaneers to get each player's team
-    left_join(players, by = "player_id") %>% 
-    # Group by game and player, (team and shots are held consistent)
-    group_by(game_id, player_id, team, shots) %>% 
-    # Calculate summary stats
-    summarise(total_points = sum(points_scored),
-              ones = sum((points_scored == 1)),
-              twos = sum((points_scored == 2)),
-              threes = sum((points_scored == 3)),
-              impossibles = sum((points_scored > 3)),
-              paddle_points = sum(points_scored* (paddle | foot)),
-              clink_points = sum(points_scored*clink),
-              points_per_round = total_points / last(shots),
-              off_ppr = sum(points_scored * !(paddle | foot))/ last(shots),
-              def_ppr = paddle_points/last(shots),
-              toss_efficiency = sum(!(paddle | foot ))/last(shots)) %>% 
-    ungroup()
-}
+
 
 parse_round_num = function(round){
   str_replace_all(round, c("([0-9]+)A" = "(\\1*2)-1",
