ffee91ac2635c331e75c4b392dd5dcb099f41b59
mdewey131 <michael.dewey131@gmail.com>
2021-06-17 21:58:28 -0700

    updated function arguments

--- R/player_selectize_ui.R
+++ R/player_selectize_ui.R
@@ -2,11 +2,10 @@
 #' Adds selectize inputs for players on both teams 
 #' @param id The namespace of the app, as well as the positional ID of the player (e.g. 'A1')
 #' @param header The header to go over the selectize input, usually "Player __"
-#' @param replacement_name A name which is handed off from 'game_restart_outputs' in the event that the game is restarted
-#' 
+#' @param player_choices The group of players to be sourced as options for the selectize
 
-player_selectize_UI = function(id, header, replacement_name) {
+player_selectize_UI = function(id, header, player_choices) {
   ns <- NS(id)
-  selectizeInput(ns('player_input'), header, c(`Player Name`='', player_choices),  
+  selectizeInput(ns('player_name'), header, c(`Player Name`='', player_choices),  
                  options = list(create = TRUE, hideSelected=T), width = "125%")
 }
