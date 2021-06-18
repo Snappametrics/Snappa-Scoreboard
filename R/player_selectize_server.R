9ccb67208fc55d0169b1bbdd4d6b8dbf1e845489
mdewey131 <michael.dewey131@gmail.com>
2021-06-17 21:59:22 -0700

    Adds function for player_selectize_server, with observers to attempt to handle updating in the event of a restarted game

--- R/player_selectize_server.R
+++ R/player_selectize_server.R
@@ -6,5 +6,27 @@
 #' @param restart_game A boolean passed from restart_game_server
 #' @param restart_input Name of the player to be updated into the selectize when the game is restarted
 #' 
-#' @return a reactive value containing the string in the selectize box
-#' 
+#' @return 
+#' \describe{
+#'      \item{player_name} {a reactive value containing the string in the selectize box}
+#' }
+
+player_selectize_server = function(id, restart_game, restart_input) {
+  moduleServer(id,
+               function(input, output, session) {
+                 ns <- session$ns
+                 player_name <- reactive({
+                   input$player_name
+                 })
+                 
+                 observeEvent(restart_game, {
+                   # Is there a reason to do this over just writing 'restart_game'?
+                   req(isTRUE(restart_game))
+                   updateSelectizeInput(session = getDefaultReactiveDomain(),
+                                        inputId = 'player_name',
+                                        selected = restart_input)
+                   
+                 })
+                 
+               })
+}
