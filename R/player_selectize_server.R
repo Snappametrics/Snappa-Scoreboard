532d7c9374704889d782b00594b8c999f6a89e3c
mdewey131 <michael.dewey131@gmail.com>
2021-06-17 21:23:22 -0700

    Adds the player selectize server module

---
+++ R/player_selectize_server.R
@@ -0,0 +1,10 @@
+#' Player selectize server
+#' Holds the reactive player name from the corresponding player selectize UI
+#' and receives from restart game ui to update this value 
+#' 
+#' @param id namespace of the module
+#' @param restart_game A boolean passed from restart_game_server
+#' @param restart_input Name of the player to be updated into the selectize when the game is restarted
+#' 
+#' @return a reactive value containing the string in the selectize box
+#' 
