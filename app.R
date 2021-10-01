31188eb8813a7d7f6beacd9ed0c1f29c0ea0a5c4
MokeEire <markkbarrett@gmail.com>
2021-09-30 18:35:00 -0700

    Easter eggs

--- app.R
+++ app.R
@@ -42,13 +42,13 @@ source("markov/Markov_model_functions.R")
 rounds = str_c(rep(1:100, each = 2), rep(c("A", "B"), 100))
 round_labels = rep(c("Pass the dice", "Next round"),100)
 
-casualty_rules = tribble(~team_A, ~team_B, ~casualty_title, ~casualty_text,
-                         12, 7, "12-7", "Roll off to see who is taking the kamikaze to the face",
-                         7, 12, "12-7", "Roll off to see who is taking the kamikaze to the face",
-                         18, 12, "War of 1812", "Everyone roll a die, the lowest roll takes a shot.",
-                         12, 18, "War of 1812", "Everyone roll a die, the lowest roll takes a shot.",
-                         20, 03, "2003", "Nevar forget: a 9/11 consists of a shot of fireball into a Sam Adams",
-                         03, 20, "2003", "Nevar forget: a 9/11 consists of a shot of fireball into a Sam Adams")
+casualty_rules = tribble(~team_A, ~team_B, ~casualty_title, ~casualty_text, ~casualty_image,
+                         12, 7, "12-7", "Roll off to see who is taking the kamikaze to the face", "https://cdn.britannica.com/96/192896-131-ECCA4FB7/Explosion-USS-Shaw-magazine-attack-Pearl-Harbor-Dec-7-1941.jpg",
+                         7, 12, "12-7", "Roll off to see who is taking the kamikaze to the face", "https://cdn.britannica.com/96/192896-131-ECCA4FB7/Explosion-USS-Shaw-magazine-attack-Pearl-Harbor-Dec-7-1941.jpg",
+                         18, 12, "War of 1812", "Everyone roll a die, the lowest roll takes a shot.", "https://thumbs-media.smithsonianmag.com/filer/32/82/32827c55-c567-4508-ae60-8fe37b647fc6/42-24017617-1.jpg__1072x720_q85_crop.jpg",
+                         12, 18, "War of 1812", "Everyone roll a die, the lowest roll takes a shot.", "https://thumbs-media.smithsonianmag.com/filer/32/82/32827c55-c567-4508-ae60-8fe37b647fc6/42-24017617-1.jpg__1072x720_q85_crop.jpg",
+                         20, 03, "2003", "Nevar forget: a 9/11 consists of a shot of fireball into a Sam Adams", "https://img.memecdn.com/7-10-amp-quot-planes-don-amp-039-t-go-through-buildings-amp-quot---osama-bin-laden_o_3264599.gif",
+                         03, 20, "2003", "Nevar forget: a 9/11 consists of a shot of fireball into a Sam Adams", "https://img.memecdn.com/7-10-amp-quot-planes-don-amp-039-t-go-through-buildings-amp-quot---osama-bin-laden_o_3264599.gif")
 
 # DB Tables ---------------------------------------------------------------
 
--- ui_functions.R
+++ ui_functions.R
@@ -130,11 +130,12 @@ casualty_popup = function(session, score, rules, players = snappaneers()$player_
                     title = current_rule$casualty_title,
                     text = current_rule$casualty_text,
                     # Want to add images, but these don't work yet
-                    # url = 'https://cdn.britannica.com/96/192896-131-ECCA4FB7/Explosion-USS-Shaw-magazine-attack-Pearl-Harbor-Dec-7-1941.jpg',
-                    # imageWidth = 400,
-                    # imageHeight = 200,
-                    type = "warning",
+                    imageUrl = current_rule$casualty_image,
+                    imageWidth = 400,
+                    imageHeight = 200,
+                    type = NULL,
                     input = "radio",
+                    width = "50%",
                     inputOptions = players)
   } else {
     invisible()
