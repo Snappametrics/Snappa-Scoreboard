2cc1b0ea96987b224f909dd9505850e3f354c3fd
MokeEire <markkbarrett@gmail.com>
2021-11-19 16:03:23 -0800

    Move playerSelectize modules into playerInput

--- R/playerInputServer.R
+++ R/playerInputServer.R
@@ -4,6 +4,27 @@
 #' 
 #' @param id The namespace of the module
 #' 
+#' Player selectize server
+#' Holds the reactive player name from the corresponding player selectize UI
+#' and receives from restart game ui to update this value 
+#' 
+#' @param id namespace of the module
+#' @param restart_game A boolean passed from restart_game_server
+#' @param restart_input Name of the player to be updated into the selectize when the game is restarted
+#' 
+#' @return 
+#' \describe{
+#'      \item{player_name} {a reactive value containing the string in the selectize box}
+#' }
+
+playerSelectizeServer = function(id, restart_input) {
+  moduleServer(id,
+               function(input, output, session) {
+                 reactive(input$name)
+                 
+               })
+}
+
 teamInputServer = function(id){
   team = str_sub(id, -1, -1)
   ns = NS(id)
