a956e933e9f4bb00ff25e0ba28ccb83edccae0ff
MokeEire <markkbarrett@gmail.com>
2023-06-08 22:05:45 -0700

    Exclude more folders when deploying

--- documentation/deployapp.R
+++ documentation/deployapp.R
@@ -1,5 +1,5 @@
 rsconnect::deployApp(
   appName = "APP_NAME_HERE",
   appFiles = list.files(recursive = T) %>% 
-    str_subset("(documentation|testing|analysis)/*", negate=T)
+    str_subset("(database|r\\-lib|renv|documentation|testing|analysis)/*", negate=T)
 )
